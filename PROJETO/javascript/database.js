ab24f5c5523f79c6c488738046dea141ee6e3e3b
seanderiko <160286408+seandeiko@users.noreply.github.com>
2024-05-06 20:31:28 -0300

    Update database.js

--- PROJETO/javascript/database.js
+++ PROJETO/javascript/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 //criando uma instancia do Sequelize
 //esta instancia é uma conexão com o banco MYSQL
-const connection = new Sequelize("sue2", "root", "", {
+const connection = new Sequelize("sue", "root", "", {
     host: "localhost",
     dialect: "mysql",
 });
