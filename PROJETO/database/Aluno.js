5e13ca812ffe8b3a6cb22d59464656111c6ec3fd
seanderiko <160286408+seandeiko@users.noreply.github.com>
2024-06-03 19:26:20 -0300

    Update Aluno.js

--- PROJETO/database/Aluno.js
+++ PROJETO/database/Aluno.js
@@ -1,8 +1,7 @@
-// Importar os módulos necessários utilizados pelo SEQUELIZE
-const { DataTypes, Sequelize } = require("sequelize");
+const { DataTypes } = require("sequelize");
 const connection = require("./database");
+const Usuarios = require("./Usuarios");
 
-// Definição do modelo (MODEL) que corresponde à uma tabela do banco de dados.
 const Alunos = connection.define(
     "aluno",
     {
@@ -10,26 +9,30 @@ const Alunos = connection.define(
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
+            autoIncrement: true
         },
         usuario_id: {
             type: DataTypes.INTEGER,
-            foreignKey: true,
-
+            allowNull: false,
+            references: {
+                model: Usuarios,
+                key: 'usuario_id'
+            }
         },
         nome: {
             type: DataTypes.TEXT,
-
+            allowNull: false
         },
         data_nascimento: {
-            type: DataTypes.DATE,
+            type: DataTypes.DATE
         },
         genero: {
             type: DataTypes.ENUM('masculino', 'feminino', 'Outro'),
-            allowNull: false,
+            allowNull: false
         },
         endereco: {
             type: DataTypes.TEXT
-        },
+        }
     },
     {
         timestamps: false
@@ -38,15 +41,15 @@ const Alunos = connection.define(
 
 async function sincronizarAluno() {
     try {
-      await Aluno.sync({ force: false });
+        await Alunos.sync({ force: false });
     } catch (error) {
-      console.error("Erro ao sincronizar a tabela: ", error);
+        console.error("Erro ao sincronizar a tabela: ", error);
     } finally {
-      await connection.close();
-      console.log("Conexão fechada.");
+        await connection.close();
+        console.log("Conexão fechada.");
     }
-  }
+}
 
-  Alunos.sync({ force: false }).then(() => {});
+Alunos.sync({ force: false }).then(() => {});
 
-  module.exports = Alunos;
+module.exports = Alunos;
