bccbe79b5971257581f4ed2c5a5fb560f3b5f5d0
seanderiko <160286408+seandeiko@users.noreply.github.com>
2024-06-03 19:30:43 -0300

    Update Presenca.js

--- PROJETO/database/Presenca.js
+++ PROJETO/database/Presenca.js
@@ -1,8 +1,8 @@
-// Importar os módulos necessários utilizados pelo SEQUELIZE
-const { DataTypes, Sequelize } = require("sequelize");
+const { DataTypes } = require("sequelize");
 const connection = require("./database");
+const Aluno = require("./Aluno"); // Assuming Aluno model is defined in "Aluno.js" file
+const Turma = require("./Turma"); // Assuming Turma model is defined in "Turma.js" file
 
-// Definição do modelo (MODEL) que corresponde à uma tabela do banco de dados.
 const Presenca = connection.define(
     "presenca",
     {
@@ -10,20 +10,32 @@ const Presenca = connection.define(
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
+            autoIncrement: true // Assuming this should be auto-incremented
         },
         aluno_id: {
             type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Aluno, // Reference the Aluno model
+                key: 'id_aluno'
+            }
         },
         turma_id: {
             type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Turma, // Reference the Turma model
+                key: 'turma_id'
+            }
         },
         data: {
             type: DataTypes.DATE,
+            allowNull: false
         },
         presente: {
             type: DataTypes.BOOLEAN,
             allowNull: false
-          },
+        },
     },
     {
         timestamps: false
@@ -39,8 +51,8 @@ async function sincronizarPresenca() {
       await connection.close();
       console.log("Conexão fechada.");
     }
-  }
+}
 
-  Presenca.sync({ force: false }).then(() => {});
+Presenca.sync({ force: false }).then(() => {});
 
-  module.exports = Presenca;
+module.exports = Presenca;
