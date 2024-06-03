8f0f4fda74d5a788deb344f5b4e8114a93b81bec
seanderiko <160286408+seandeiko@users.noreply.github.com>
2024-06-03 19:30:09 -0300

    Update Nota.js

--- PROJETO/database/Nota.js
+++ PROJETO/database/Nota.js
@@ -1,27 +1,45 @@
-// Importar os módulos necessários utilizados pelo SEQUELIZE
-const { DataTypes, Sequelize } = require("sequelize");
+const { DataTypes } = require("sequelize");
 const connection = require("./database");
+const Aluno = require("./Aluno"); // Assuming Aluno model is defined in "Aluno.js" file
+const Turma = require("./Turma"); // Assuming Turma model is defined in "Turma.js" file
+const Disciplina = require("./Disciplina"); // Assuming Disciplina model is defined in "Disciplina.js" file
 
-// Definição do modelo (MODEL) que corresponde à uma tabela do banco de dados.
 const Nota = connection.define(
     "nota",
     {
         nota_id: {
-          type: DataTypes.INTEGER,
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true // Assuming this should be auto-incremented
         },
         aluno_id:{
-          type: DataTypes.INTEGER,
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Aluno, // Reference the Aluno model
+                key: 'id_aluno'
+            }
         },
         turma_id:{
-          type: DataTypes.INTEGER,
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Turma, // Reference the Turma model
+                key: 'turma_id'
+            }
         },
         disciplina_id:{
-          type: DataTypes.INTEGER,
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Disciplina, // Reference the Disciplina model
+                key: 'disciplina_id'
+            }
         },
         nota:{
-          type: DataTypes.FLOAT,
-        },
-
+            type: DataTypes.FLOAT,
+            allowNull: false
+        }
     },
     {
         timestamps: false
@@ -37,8 +55,8 @@ async function sincronizarNota() {
       await connection.close();
       console.log("Conexão fechada.");
     }
-  }
+}
 
-  Nota.sync({ force: false }).then(() => {});
+Nota.sync({ force: false }).then(() => {});
 
-  module.exports = Nota;
+module.exports = Nota;
