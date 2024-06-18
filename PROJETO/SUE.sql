6d0efd9a17e9d1a94e46cc87b41678ea646d43f4
seanderiko <160286408+seandeiko@users.noreply.github.com>
2024-06-18 19:46:20 -0300

    Update SUE.sql

--- PROJETO/SUE.sql
+++ PROJETO/SUE.sql
@@ -1,82 +1,114 @@
-CREATE DATABASE SUE;
+CREATE TABLE `alunos` (
+  `aluno_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `genero` enum('Masculino','Feminino','Outro') NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  PRIMARY KEY (`aluno_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
-USE SUE;
+CREATE TABLE `coordenadores` (
+  `coordenador_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `genero` enum('Masculino','Feminino','Outro') NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  PRIMARY KEY (`coordenador_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE Professores (
-    professor_id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    data_nascimento DATE,
-    genero ENUM('Masculino', 'Feminino', 'Outro'),
-    endereco VARCHAR(255)
-);
+CREATE TABLE `coordenadors` (
+  `coordenador_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` text NOT NULL,
+  `data_nascimento` datetime DEFAULT NULL,
+  `genero` enum('masculino','feminino','Outro') NOT NULL,
+  `endereco` text DEFAULT NULL,
+  PRIMARY KEY (`coordenador_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE Coordenadores (
-    coordenador_id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    data_nascimento DATE,
-    genero ENUM('Masculino', 'Feminino', 'Outro'),
-    endereco VARCHAR(255)
-);
+CREATE TABLE `cronograma` (
+  `evento_id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) NOT NULL,
+  `data_inicio` date NOT NULL,
+  `data_fim` date NOT NULL,
+  `tipo` enum('Aula','Prova','Feriado','Recesso') NOT NULL,
+  `professor_id` int(11) NOT NULL,
+  PRIMARY KEY (`evento_id`),
+  KEY `professor_id` (`professor_id`),
+  CONSTRAINT `cronograma_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `professores` (`professor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE Alunos (
-    aluno_id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    data_nascimento DATE,
-    genero ENUM('Masculino', 'Feminino', 'Outro'),
-    endereco VARCHAR(255)
-);
+CREATE TABLE `disciplina` (
+  `disciplina_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_disciplina` varchar(50) NOT NULL,
+  `carga_horaria` int(11) NOT NULL,
+  `descricao_disciplina` varchar(100) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`disciplina_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE Disciplinas (
-    disciplina_id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    codigo VARCHAR(20),
-    carga_horaria INT
-);
+CREATE TABLE `disciplinas` (
+  `disciplina_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `codigo` varchar(20) NOT NULL,
+  `carga_horaria` int(11) NOT NULL,
+  PRIMARY KEY (`disciplina_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE Turmas (
-	turma_id INT PRIMARY KEY AUTO_INCREMENT,
-	nome_turma TEXT,
-    turno ENUM ('Manhã', 'Tarde', 'Noite'),
-    ano YEAR,
-    semestre ENUM('primeiro', 'segundo')
-);
+CREATE TABLE `notas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `aluno_id` int(11) NOT NULL,
+  `disciplina_id` int(11) NOT NULL,
+  `professor_id` int(11) NOT NULL,
+  `data_nota` datetime NOT NULL,
+  `nota` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `aluno_id` (`aluno_id`),
+  KEY `disciplina_id` (`disciplina_id`),
+  KEY `professor_id` (`professor_id`),
+  CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`aluno_id`) REFERENCES `alunos` (`aluno_id`),
+  CONSTRAINT `notas_ibfk_2` FOREIGN KEY (`disciplina_id`) REFERENCES `disciplina` (`disciplina_id`),
+  CONSTRAINT `notas_ibfk_3` FOREIGN KEY (`professor_id`) REFERENCES `professores` (`professor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE Notas (
-    aluno_id INT,
-    disciplina_id INT,
-    professor_id INT,
-    data_nota DATE,
-    nota FLOAT,
-    PRIMARY KEY (aluno_id, disciplina_id, professor_id),
-    FOREIGN KEY (aluno_id) REFERENCES Alunos(aluno_id),
-    FOREIGN KEY (disciplina_id) REFERENCES Disciplinas(disciplina_id),
-    FOREIGN KEY (professor_id) REFERENCES Professores(professor_id)
-);
+CREATE TABLE `presencas` (
+  `presenca_id` int(11) NOT NULL AUTO_INCREMENT,
+  `aluno_id` int(11) NOT NULL,
+  `turma_id` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `presente` tinyint(1) NOT NULL,
+  PRIMARY KEY (`presenca_id`),
+  KEY `aluno_id` (`aluno_id`),
+  KEY `turma_id` (`turma_id`),
+  CONSTRAINT `presencas_ibfk_1` FOREIGN KEY (`aluno_id`) REFERENCES `alunos` (`aluno_id`),
+  CONSTRAINT `presencas_ibfk_2` FOREIGN KEY (`turma_id`) REFERENCES `turmas` (`turma_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE Presencas (
-    presenca_id INT PRIMARY KEY AUTO_INCREMENT,
-    aluno_id INT,
-    turma_id INT,
-    data DATE,
-    presente BOOLEAN,
-    FOREIGN KEY (aluno_id) REFERENCES Alunos(aluno_id),
-    FOREIGN KEY (turma_id) REFERENCES Turmas(turma_id)
-);
+CREATE TABLE `professordisciplina` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `professor_id` int(11) NOT NULL,
+  `disciplina_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `professor_id` (`professor_id`),
+  KEY `disciplina_id` (`disciplina_id`),
+  CONSTRAINT `professordisciplina_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `professores` (`professor_id`),
+  CONSTRAINT `professordisciplina_ibfk_2` FOREIGN KEY (`disciplina_id`) REFERENCES `disciplinas` (`disciplina_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE ProfessorDisciplina (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    professor_id INT,
-    FOREIGN KEY (professor_id) REFERENCES Professores(professor_id),
-    disciplina_id INT,
-    FOREIGN KEY (disciplina_id) REFERENCES Disciplinas(disciplina_id)
-);
+CREATE TABLE `professores` (
+  `professor_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `genero` enum('Masculino','Feminino','Outro') NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  PRIMARY KEY (`professor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE Cronograma (
-    evento_id INT PRIMARY KEY AUTO_INCREMENT,
-    descricao VARCHAR(255),
-    data_inicio DATE,
-    data_fim DATE,
-    tipo ENUM('Aula', 'Prova', 'Feriado', 'Recesso'),
-    professor_id INT,
-    FOREIGN KEY (professor_id) REFERENCES Professores(professor_id)
-);
+CREATE TABLE `turmas` (
+  `turma_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_turma` text NOT NULL,
+  `turno` enum('Manhã','Tarde','Noite') NOT NULL,
+  `ano` year(4) NOT NULL,
+  `semestre` enum('primeiro','segundo') NOT NULL,
+  PRIMARY KEY (`turma_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
